bc91c41eae2092ee310ef63a92715de4d998fe0c
geovane-nves <geonevesrodrigues@gmail.com>
2025-12-07 09:28:06 -0300

    pop-up

--- backend/home/static/home/Script.js
+++ backend/home/static/home/Script.js
@@ -14,3 +14,21 @@ function nextImage(){
 
     document.getElementById("radio"+count).checked = true;
 }
+
+
+
+const modal = document.getElementById("modal");
+const openBtn = document.getElementById("openmodal");
+const closeBtn = document.getElementById("closeModal");
+
+openBtn.onclick = () => {
+    modal.style.display = "flex";
+}
+
+closeBtn.onclick = () => {
+    modal.style.display = "none";
+}
+
+window.onclick = (e) => {
+    if (e.target === modal) modal.style.display = "none";
+}
