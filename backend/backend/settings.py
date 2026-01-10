02c628892e60032f36fa24f1cd9b0ec7cc3ef057
Athlas <elderluiz72@gmail.com>
2026-01-10 15:32:05 -0300

    aplying css on deploy

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -69,6 +69,7 @@ SOCIALACCOUNT_EMAIL_VERIFICATION = False
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -146,6 +147,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
