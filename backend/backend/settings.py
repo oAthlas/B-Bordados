7aef3709cde50de72224d9ec4a83f181a7c1a9f8
Athlas <elderluiz72@gmail.com>
2026-01-13 23:27:17 -0300

    database completely migrated

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -110,13 +110,13 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-
 DATABASES = {
-        "default": {
-            "ENGINE": "django.db.backends.sqlite3",
-            "NAME": BASE_DIR / "db.sqlite3",
-        }
-    }
+    "default": dj_database_url.parse(
+        os.environ.get("DATABASE_URL"),
+        conn_max_age=600,
+        ssl_require=True
+    )
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
