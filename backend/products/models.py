3935a2c5eec22432c46198fa3bfcac857ff99d8d
Athlas <elderluiz72@gmail.com>
2026-01-14 13:28:10 -0300

    i've already lost my mind on this

--- backend/products/models.py
+++ backend/products/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 
 class Category(models.Model):
     name = models.CharField(max_length=100, unique=True)
@@ -12,6 +13,9 @@ class Product(models.Model):
     description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
     image = models.ImageField(upload_to='products/', null=True, blank=True)
+
+    def get_absolute_url(self):
+        return reverse("product_detail", args=[self.id])
 
     file = models.FileField(upload_to='product_archives/', null=True, blank=True)
 
