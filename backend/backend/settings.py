45fad4cea38bba6f9028545f70aadcc52e9b8945
Athlas <elderluiz72@gmail.com>
2026-01-13 22:33:33 -0300

    backup sqlite before up on supabase

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -110,16 +110,8 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-if os.environ.get("DATABASE_URL"):
-    DATABASES = {
-        "default": dj_database_url.parse(
-            os.environ["DATABASE_URL"],
-            conn_max_age=600,
-            ssl_require=True
-        )
-    }
-else:
-    DATABASES = {
+
+DATABASES = {
         "default": {
             "ENGINE": "django.db.backends.sqlite3",
             "NAME": BASE_DIR / "db.sqlite3",
